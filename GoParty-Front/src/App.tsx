8a791e861c7c86c435ef6443e97757e464e1f3c8
Pedro Aluisio <121454190+pedrinGrando@users.noreply.github.com>
2024-03-11 22:01:56 -0300

    :heavy_plus_sign: Instalacao do Tailwindcss

--- GoParty-Front/src/App.tsx
+++ GoParty-Front/src/App.tsx
@@ -1,14 +1,17 @@
-import './App.css'
+import React from 'react'
+import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // Fixar aqui qual componente desejado para ser renderizado como default
 import Home from './pages/public/home/Home'
+import Login from './pages/public/login/Login'
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home></Home>} />
+        <Route path='/' element={<Login></Login>} />
         {/* Rotas de navegação do sistema */}
         {/*ROTA2= <Route path='/' element={<Home></Home>} /> */}
         {/*ROTA3= <Route path='/' element={<Home></Home>} /> */}
--- GoParty-Front/vite.config.ts
+++ GoParty-Front/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import WindiCSS from 'vite-plugin-windicss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    WindiCSS({
+      // Adicione quaisquer opções de configuração necessárias aqui
+    }),
+  ],
+});
