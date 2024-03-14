f7ae8ed7e363b521c2450162df4ecb5fcb00543b
Renan-Leal <renan.leallol@gmail.com>
2024-03-13 21:09:33 -0300

    :lipstick: Inicialização da estilização da home

--- GoParty-Front/src/pages/public/home/Home.tsx
+++ GoParty-Front/src/pages/public/home/Home.tsx
@@ -1,17 +1,16 @@
+import { Footer } from "../../../components/Footer/Footer";
+import { NavBar } from "../../../components/NavBar/NavBar";
 import { useUser } from "../../../components/UserContext/UserContext";
+import { Sidebar } from "../../../components/sidebar/Sidebar";
 
 export default function Home () {
     const { user } = useUser();
 
     return (
         <div>
-            <h1>
-                Home em Construção
-                {user && <p>Logged in as: {user.username}</p>}
-            </h1>
-            <span>A home deve ser a página principal, nela, deve ter uma apresentação da GoParty. Posteriormente, 
-            Após o usuário criar em entrar, será redirecionado para o login e sómente de pois será redirecionado para a HOME DO SISTEMA.
-            </span>
+            <NavBar /> 
+            <Sidebar userName={user?.username}/>
+            <Footer />
         </div>
     )
 }
