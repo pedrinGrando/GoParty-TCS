ece314a906dae6f2bbe579c705ec6e6b7aea61d3
marcus <marcusvcf04@gmail.com>
2024-04-02 00:21:29 -0300

    :construction: feat: Contrução de serviço responsavel por gerar e validar o token JWT

---
+++ GoParty-API/tcs/src/main/java/go/party/tcs/service/JWTService.java
@@ -0,0 +1,50 @@
+package go.party.tcs.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import go.party.tcs.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class JWTService {
+
+    @Value("${token.secret}")
+    private String secret;
+
+    public String generateToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("GoPartySystem")
+                    .withSubject(usuario.getUsername())
+                    .withExpiresAt(this.getExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao autenticar o usuario");
+        }
+    }
+
+    private Instant getExpirationDate() {
+        return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.ofHours(-3));
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("GoPartySystem")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+}
