423a0979254294499db51c0ef0448bb160eb46cb
Caue Bastos <caueabastos@gmail.com>
2024-04-24 21:01:01 -0300

    :construction: requisicao para formatura

---
+++ GoParty-Front/src/types/Graduation.ts
@@ -0,0 +1,15 @@
+export interface Graduation {
+  titulo: string;
+  descricao: string;
+  estado: string;
+  rua: string;
+  cidade: string;
+  cep: string;
+  bairro: string;
+  metaArrecad: string;
+  arrecadado: string;
+  dataPrevista: Date;
+  dataSolicitacao: Date;
+  chavePix: string;
+  formaturaCaminho: string;
+}
