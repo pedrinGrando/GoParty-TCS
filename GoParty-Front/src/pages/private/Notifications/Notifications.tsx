731bdab44d89be7859df61d9ad1061bd571be3df
Renan Leal <104607462+Renan-Leal@users.noreply.github.com>
2024-06-05 20:24:12 -0300

    :dizzy: Visual Invites

--- GoParty-Front/src/pages/private/Notifications/Notifications.tsx
+++ GoParty-Front/src/pages/private/Notifications/Notifications.tsx
@@ -171,17 +171,17 @@ export default function Notifications() {
                                     <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 1v5h-5M2 19v-5h5m10-4a8 8 0 0 1-14.947 3.97M1 10a8 8 0 0 1 14.947-3.97"/>
                                     </svg>
-                                    <span className="sr-only">Refresh icon</span>
+                                    <span className="sr-only">Convite para formatura</span>
                                 </div>
                                 <div className="ms-3 text-sm font-normal">
-                                    <span className="mb-1 text-sm font-semibold text-gray-900 dark:text-white">Update available</span>
-                                    <div className="mb-2 text-sm font-normal">A new software version is available for download.</div> 
+                                    <span className="mb-1 text-sm font-semibold text-gray-900 dark:text-white">{invite.inviteDate}</span>
+                                    <div className="mb-2 text-sm font-normal"></div> 
                                     <div className="grid grid-cols-2 gap-2">
                                         <div>
-                                            <a href="#" className="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Update</a>
+                                            <a href="#" className="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Aceitar</a>
                                         </div>
                                         <div>
-                                            <a href="#" className="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Not now</a> 
+                                            <a href="#" className="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Rejeitar</a> 
                                         </div>
                                     </div>    
                                 </div>
