185bac988518047b5ebf5df15572ebcdd17a23e9
Renan Leal <104607462+Renan-Leal@users.noreply.github.com>
2024-06-26 21:07:43 -0300

    :dizzy: Visual ingressos

--- GoParty-Front/src/pages/private/Tickets/Tickets.tsx
+++ GoParty-Front/src/pages/private/Tickets/Tickets.tsx
@@ -60,7 +60,7 @@ export default function Tickets() {
 
                             <div className="max-w-3xl mx-auto mt-5 bg-white shadow-md rounded-lg flex flex-col md:flex-row border border-gray-300">
                                 <div className="flex-shrink-0 w-full md:w-1/3 bg-gray-100 p-4 flex items-center justify-center rounded-t-lg md:rounded-l-lg md:rounded-t-none">
-                                    <img src={"qrCodeUrl"} alt="QR Code" className="w-24 h-24 object-cover" />
+                                    <img src={`https://api.qrserver.com/v1/create-qr-code/?size=450x450&data=${ingresso.codigoEvento}`} alt="QR Code" className="w-24 h-24 object-cover" />
                                 </div>
                                 <div className="w-full md:w-2/3 p-4">
                                     <div className="flex flex-col md:flex-row justify-between mb-2">
