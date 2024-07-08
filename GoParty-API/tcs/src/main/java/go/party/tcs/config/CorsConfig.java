13dfb6a17556757f0205f9966817ea47be2cd7b9
Marcus Ferreira <marcusvcf04@gmail.com>
2024-07-08 16:59:51 -0300

    :wrench: chore: Adicionando configuracao de CORS

--- GoParty-API/tcs/src/main/java/go/party/tcs/config/CorsConfig.java
+++ GoParty-API/tcs/src/main/java/go/party/tcs/config/CorsConfig.java
@@ -2,6 +2,7 @@ package go.party.tcs.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,10 +10,16 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS") 
-                .allowedHeaders("*") 
-                .allowCredentials(true); 
+        registry
+            .addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("file:uploads/");
     }
 }
